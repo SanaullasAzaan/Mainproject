2cc96f97eef21af0b8c1e527ea0fbffc63f436db
SanaullasAzaan <97332063+SanaullasAzaan@users.noreply.github.com>
2024-03-26 23:26:55 +0530

    bannercompleted

---
+++ ssithub/src/components/appbar/banner/index.js
@@ -0,0 +1,27 @@
+import { useTheme } from "@emotion/react";
+import { Typography, useMediaQuery } from "@mui/material";
+import { BannerContainer, BannerContent, BannerTitle,BannerDescription, BannerImage} from "../../../styles/banner";
+// Example import in src/components/appbar/banner/index.js
+
+
+export default function Banner(){
+    const theme=useTheme();
+    const matches=useMediaQuery(theme.breakpoints.down('md'));
+
+    return(
+        <BannerContainer>
+            <BannerImage src="/images/banner/banner.jpg"/>
+            <BannerContent>
+            <Typography variant="h6">New Platform</Typography>
+            <BannerTitle variant="h2">
+                Title
+            </BannerTitle>
+
+            <BannerDescription variant="subtitle">
+            put some stuff regarding our project
+            </BannerDescription>
+            </BannerContent>
+        </BannerContainer>
+        
+    )
+}
